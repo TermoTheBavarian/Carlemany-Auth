7db16bdbbb17c693515a2387bcb7d8d180ffac50
German Gomez Marques <German.Gomez@ricoh.es>
2024-07-07 17:47:34 +0200

    Practica 3 Auth

---
+++ auth-carlemany/resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="https://images.griddo.universitatcarlemany.com/c/cover/q/65/f/svg/ucma-logo-no-text" alt="">
---
+++ auth-carlemany/resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased font-sans">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" style="color: #001c47" />
+            <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white" style="background-color: #001c47">
+                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div class="flex lg:justify-center lg:col-start-2">
+                            <img src="https://images.griddo.universitatcarlemany.com/c/cover/q/65/f/svg/ucma-logo-no-text" alt="">
+                        </div>
+                        @if (Route::has('login'))
+                            <livewire:welcome.navigation />
+                        @endif
+                    </header>
+
+                    <main class="mt-6">
+                        <div >
+                            <div class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800">
+
+                                <div class="pt-3 sm:pt-5">
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">Authentication service design and implementation</h2>
+
+                                    <p class="mt-4 text-sm/relaxed">
+                                        This activity consists of designing a full authentication service that can be used in any application
+                                        and that includes all the typical forms of authentication and identification, such as normal
+                                        passwords, OTP, certificate and OAuth2.0. Once designed, you will have to implement one of
+                                        the methods (or at least a part of it).
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
